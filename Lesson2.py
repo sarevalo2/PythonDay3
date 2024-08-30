787482f500cd4257eed9e7236c3f5aa2086cbc9d
Arevalo <sarevalo2@cps.k12.il.us>
2024-08-30 15:06:30 -0500

    first commit

---
+++ Lesson2.py
@@ -0,0 +1,55 @@
+#create a print function that prints 
+# out the sreing "Hello, World"
+# print ("Hello World")
+# ask the user for the day of the week
+# day_of_week= input("What day of the week is it")
+# print ("Today is"+ day_of_week)
+# concentration is when you add two strings together
+# using a plus sigm (+)
+# movies_this_week = input ("What movies are you watching this week?")
+# print ("I am watching"+ movies_this_week + "this week")
+# mood= input ("How are you feeling today? ")
+# print ("I am feeling" + mood ")
+
+# data types for variables in python
+# strings - text
+name= "John" # this is a string data type 
+#whenever it is wrapped in quotes it is a string
+year = "2024"
+# integers - whole numbers
+year =  2024 # this is an interger data type
+# does not need to be wrapped in quotes
+# yearFourFromNow = 2028
+# subtract = yearFourFromNow - year
+# print(subtract)
+# # floats - decimal numbers
+# priceBigMac= 3.99 # this is a float data type
+# priceDoublePounder= 4.99 
+# totalprice= priceBigMac + priceDoublePounder
+# print(totalprice)
+# # booleans - True or False
+# isRaining = False # this is a boolean data type
+# print(isRaining)
+# # lists - a collection of items
+# groceries = ["apples", "bananas" , "carrots"]
+# print(groceries)
+#challenge #1
+movielist= ("deadpool" , "avengers" , "Bee Movie")
+print(movielist)
+RestaurantList= ("KFC" , "Popeyes" , "Harolds Chicken")
+print(RestaurantList)
+AdressHarolds= (" 1521 S Pulaski Rd, Chicago, IL 60623")
+print(AdressHarolds)
+AdressKFC= ("2823 S Cicero Ave, Cicero, IL 60804")
+print(AdressKFC)
+AdressPopeyes= ("4773 W Cermak Rd, Cicero, IL 60804")
+print(AdressPopeyes)
+TimeofMovie= ("Deadpool - 7:30 P.M." , "Avengers - 8:15 P.M." , "Bee Movie - 10:00 P.M.")
+print=TimeofMovie
+DinnerTime= ("5 P.M.")
+print=DinnerTime
+peopleGoing= ("Family")
+print(Family)
+#type conversation converting from one data type to another
+# print the entire output in one sentence
+print("We are going to go see"+ TimeofMovie + "at" + TimeofMovie + "and then eat dinner at" + AdressHarolds + "at" + DinnerTime + "with" ) 
